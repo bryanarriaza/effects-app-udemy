a3b53a95bc6957f0ac8491a68d51c91a36618739
Bryan Arriaza <bryan.arriaza.g1993@gmail.com>
2019-02-10 13:29:40 -0600

    Cargando usuarios en lista de manera tradicional con servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+// NgRx
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { appReducers } from './store/app.reducer';
+
+// Environment
+import { environment } from '../environments/environment';
+
 // Rutas
 import { AppRoutingModule } from './app-routing.module';
 
@@ -16,6 +25,11 @@ import { UsuariosModule } from './usuarios/usuarios.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    StoreModule.forRoot(appReducers),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production // Restrict extension to log-only mode
+    }),
     SharedModule,
     UsuariosModule
   ],
